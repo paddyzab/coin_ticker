6d82bdcb9f940061cbefc57e5b4c8098b298b2d0
Patryk Zabicki <patryk.zabicki@gmail.com>
2016-12-07 19:16:59 +0100

    extracted common logic into client.

--- coin_ticker.go
+++ coin_ticker.go
@@ -1,35 +1,19 @@
-package main
+package ct
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
-	"github.com/dghubble/sling"
 	"github.com/urfave/cli"
 )
-
-// Coin represents data resturned from the coinmarketcap API
-type Coin struct {
-	ID               string `json:"id"`
-	Name             string `json:"name"`
-	Symbol           string `json:"symbol"`
-	Rank             int    `json:"rank"`
-	PriceUsd         string `json:"price_usd"`
-	PriceBtc         string `json:"price_btc"`
-	VolumeUsd24h     string `json:"24h_volume_usd"`
-	MarketCapUsd     string `json:"market_cap_usd"`
-	TotalSupply      string `json:"total_supply"`
-	PercentChange1h  string `json:"percent_change_1h"`
-	PercentChange24h string `json:"percent_change_24h"`
-	PercentChange7d  string `json:"percent_change_7d"`
-	LastUpdated      string `json:"last_updated"`
-}
-
-const baseURL = "https://api.coinmarketcap.com/v1/ticker/"
 
 func main() {
 
 	var coinToken string
+
+	httpClient := &http.Client{}
+	ctClient := NewClient(httpClient)
 
 	app := cli.NewApp()
 	app.Flags = []cli.Flag{
@@ -46,9 +30,9 @@ func main() {
 
 	app.Action = func(c *cli.Context) error {
 		if coinToken == "b" {
-			callBitcoin()
+			fmt.Println(ctClient.GetBitcoinPrice())
 		} else if coinToken == "e" {
-			callEther()
+			fmt.Println(ctClient.GetEtherPrice())
 		} else {
 			fmt.Println("unkown option")
 		}
@@ -57,24 +41,4 @@ func main() {
 	}
 
 	app.Run(os.Args)
-}
-
-func callEther() {
-	res := new([]Coin)
-	resp, err := sling.New().Get(baseURL).Path("ethereum").ReceiveSuccess(res)
-	if err != nil {
-		fmt.Println(err, resp)
-	} else {
-		fmt.Println((*res)[0].PriceUsd)
-	}
-}
-
-func callBitcoin() {
-	res := new([]Coin)
-	resp, err := sling.New().Get(baseURL).Path("bitcoin").ReceiveSuccess(res)
-	if err != nil {
-		fmt.Println(err, resp)
-	} else {
-		fmt.Println((*res)[0].PriceUsd)
-	}
 }
---
+++ ct.go
@@ -0,0 +1,66 @@
+package ct
+
+import (
+	"net/http"
+
+	"github.com/dghubble/sling"
+)
+
+const (
+	baseURL = "https://api.coinmarketcap.com/v1/ticker/"
+	ether   = "ethereum"
+	bitcoin = "bitcoin"
+)
+
+// Client CoinTicker api client
+type Client struct {
+	sling *sling.Sling
+}
+
+// NewClient Creates new configured Client
+func NewClient(client *http.Client) *Client {
+	return &Client{
+		sling: sling.New().Client(client).Base(baseURL),
+	}
+}
+
+// Coin represents data resturned from the coinmarketcap API
+type Coin struct {
+	ID               string `json:"id"`
+	Name             string `json:"name"`
+	Symbol           string `json:"symbol"`
+	Rank             int    `json:"rank"`
+	PriceUsd         string `json:"price_usd"`
+	PriceBtc         string `json:"price_btc"`
+	VolumeUsd24h     string `json:"24h_volume_usd"`
+	MarketCapUsd     string `json:"market_cap_usd"`
+	TotalSupply      string `json:"total_supply"`
+	PercentChange1h  string `json:"percent_change_1h"`
+	PercentChange24h string `json:"percent_change_24h"`
+	PercentChange7d  string `json:"percent_change_7d"`
+	LastUpdated      string `json:"last_updated"`
+}
+
+// GetEtherPrice returns current price of Ethereum in USD
+func (client *Client) GetEtherPrice() (string, error) {
+	coin := new([]Coin)
+	_, err := client.sling.New().Path(bitcoin).ReceiveSuccess(&coin)
+
+	if err != nil {
+		return (*coin)[0].PriceUsd, err
+	}
+
+	return (*coin)[0].PriceUsd, err
+}
+
+// GetBitcoinPrice returns current price of Bitcoin in USD
+func (client *Client) GetBitcoinPrice() (string, error) {
+	coin := new([]Coin)
+	_, err := client.sling.New().Path(ether).ReceiveSuccess(&coin)
+
+	if err != nil {
+		return (*coin)[0].PriceUsd, err
+	}
+
+	return (*coin)[0].PriceUsd, err
+}
