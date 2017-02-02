7f80f45b0847b14c469b9b5241f20341c7ed1a8c
Patryk Zabicki <patryk.zabicki@gmail.com>
2017-02-02 22:25:18 +0100

    Adding basic GUI with gocui.

--- coin_ticker.go
+++ coin_ticker.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/urfave/cli"
 	"time"
+	"github.com/jroimartin/gocui"
+	"log"
 )
 
 func main() {
@@ -24,6 +26,22 @@ func main() {
 			Usage:       "coin used for testing",
 			Destination: &coinToken,
 		},
+	}
+
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil || coinToken == "" {
+		log.Panic(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
 	}
 
 	ticker := time.NewTicker(time.Second * 15)
@@ -49,4 +67,19 @@ func main() {
 	}
 
 	app.Run(os.Args)
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
