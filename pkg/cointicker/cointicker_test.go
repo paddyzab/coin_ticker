7457ddfd260999c5b8e33c14a0b3f67d2c3fb315
Albert Agraz <albert.agraz@gmail.com>
2017-05-22 22:11:06 +0200

    WIP

--- pkg/cointicker/cointicker_test.go
+++ pkg/cointicker/cointicker_test.go
@@ -1,14 +1,100 @@
 package cointicker
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
-	"reflect"
-
 	"github.com/logrusorgru/aurora"
+	"github.com/paddyzab/coin_ticker/pkg/coinmarketcap"
+
+	"strings"
+	"time"
+
+	"github.com/paddyzab/coin_ticker/pkg/storage"
 )
 
 const testString = "test"
+
+type coinMarketCapMock struct {
+	coins  []coinmarketcap.Coin
+	errors []error
+}
+
+func (c coinMarketCapMock) GetCurrenciesQuotes(currencies ...string) ([]coinmarketcap.Coin, []error) {
+	return c.coins, c.errors
+}
+
+func TestGetFormattedPrice(t *testing.T) {
+
+	cases := []struct {
+		title  string
+		coins  []coinmarketcap.Coin
+		errors []error
+		time   time.Time
+		expRes string
+		expErr []error
+	}{
+		{
+			title: "Bitcoin",
+			coins: []coinmarketcap.Coin{coinmarketcap.Coin{
+				ID:               "bitcoin",
+				Name:             "Bitcoin",
+				Symbol:           "BTC",
+				Rank:             "1",
+				PriceUsd:         "600",
+				PriceBtc:         "1.0",
+				VolumeUsd24h:     "220",
+				MarketCapUsd:     "420",
+				TotalSupply:      "800",
+				PercentChange1h:  "0.2",
+				PercentChange24h: "7.93",
+				PercentChange7d:  "-8.13",
+				LastUpdated:      "1481134760",
+			}},
+			errors: nil,
+			time:   time.Date(2017, 12, 01, 10, 15, 0, 0, time.UTC),
+			expRes: fmt.Sprintf("10:15AM BTC: 600, ETH: , XMR: %s \nB/E ratio %f, B/M ratio %f \n\n", "", 0.0, 0.0),
+				expErr: nil,
+			//%s BTC: %s, ETH: %s, XMR: %s \nB/E ratio %f, B/M ratio %f \n\n
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.title, func(t *testing.T) {
+			ct := NewCoinTicker(&coinMarketCapMock{coins: tc.coins, errors: tc.errors}, storage.NewCache())
+			res, err := ct.GetFormattedPrice(tc.time)
+			fmt.Printf("<%s>\n", res)
+
+
+			res = strings.Replace(res, "\n", "", -1)
+			res = strings.Replace(res, " ", "", -1)
+			fmt.Printf("<%s>\n", res)
+
+	//res = "house"
+
+			if fmt.Sprintf("%s", tc.expRes) != fmt.Sprintf("%s", res) {
+				fmt.Printf("res <%s>\n", res)
+
+				for _, runeValue := range []rune(res) {
+					fmt.Printf("%#U \n", runeValue)
+				}
+
+				fmt.Printf("exp <%s>\n", tc.expRes)
+
+				for _, runeValue := range []rune(tc.expRes) {
+					fmt.Printf("%#U \n", runeValue)
+				}
+				t.Error(strings.Replace(res, "\n", " ", 100))
+			}
+
+			if !reflect.DeepEqual(err, tc.expErr) {
+				t.Error(err)
+
+			}
+		})
+	}
+}
 
 func TestDecorateRatio(t *testing.T) {
 
